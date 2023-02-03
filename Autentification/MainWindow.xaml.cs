eb96fbb30ce35ac8c2c7439d51a67a658fd99a10
Антон Конст <akonstantinov.2003.sokol@gmail.com>
2023-02-03 18:06:50 +0300

    Сделана первая часть задания

--- Autentification/MainWindow.xaml.cs
+++ Autentification/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Autentification
 {
@@ -20,25 +21,53 @@ namespace Autentification
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private int time = 15;
+        private DispatcherTimer timer;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            textBoxLogin.Text = Global.login;
+            checkInitialize();
+
+            
         }
 
-        string login = "admin";
+
+
+        string login = "admin";      
         string password = "admin";
 
         private void btnAuto_Click(object sender, RoutedEventArgs e)
         {
-            if(textBoxLogin.Text == login && passBox.Password == password)
+            if(textBoxLogin.Text == "")
+            {
+                MessageBox.Show("Введите логин", "Аутентификация",
+                  MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            if (passBox.Password == "")
+            {
+                MessageBox.Show("Введите пароль", "Аутентификация",
+                  MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            if (textBoxLogin.Text == login && passBox.Password == password)
             {
                 WindowForCode windowForCode = new WindowForCode();
 
                 windowForCode.Show();
-
+                this.Close();
 
                 Random rnd = new Random();
                 int randomeCode = rnd.Next(10000, 99999);
+
+                Global.randomCode = randomeCode;
+                Global.login = textBoxLogin.Text;
                     
                 MessageBox.Show("Код: " + randomeCode, "Аутентификация",
                     MessageBoxButton.OK, MessageBoxImage.Information); 
@@ -49,5 +78,51 @@ namespace Autentification
                     MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
+
+        private void checkInitialize()
+        {
+            if(Global.failedAttempt == 1)
+            {
+                btnAuto.Visibility = Visibility.Hidden;
+                timerStart();
+            }
+            else if(Global.failedAttempt == 2)
+            {
+
+            }
+
+        }
+
+        private void timerStart()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (time == 0)
+            {
+                timer.Stop();
+                textBoxAccessIn.Text = "";
+                btnAuto.Visibility = Visibility.Visible;
+           
+            }
+            else
+            {            
+                textBoxAccessIn.Text = "Получить новый код можно будет через " + time + " секунд";
+                time--;
+            }
+        }
+
+
+        // CAPCHA
+
+        //https://www.youtube.com/watch?v=rAnAud2sCgc
+
     }
 }
--- Autentification/WindowForCode.xaml.cs
+++ Autentification/WindowForCode.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Autentification
 {
@@ -19,9 +21,52 @@ namespace Autentification
     /// </summary>
     public partial class WindowForCode : Window
     {
+
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
         public WindowForCode()
         {
             InitializeComponent();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
+            dispatcherTimer.Tick += new EventHandler(DisTimer_Tick);
+            runTimer();   
+        }
+
+       
+        private void runTimer()
+        {
+            dispatcherTimer.Start();      
+        }
+
+    private void DisTimer_Tick(object sender, EventArgs e)
+        {
+            dispatcherTimer.Stop();
+
+            try
+            {
+                int code = Convert.ToInt32(textBoxCode.Text);
+                if (code == Global.randomCode)
+                {
+                    MessageBox.Show("Добро пожаловать ", "Аутентификация",
+                   MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+                else
+                    MessageBox.Show("Код не соответствует ", "Аутентификация",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch
+            {
+                MessageBox.Show("Код не соответствует ", "Аутентификация",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            Global.failedAttempt++;
+            
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
         }
     }
 }
